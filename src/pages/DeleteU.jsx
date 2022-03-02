39866212f48bfb43a6cb993ab42e6599e82a7f40
hosseindavoodi <h.davodi2002@gmail.com>
2022-03-02 14:23:22 +0300

    Version 0.2 added delete component

--- src/pages/DeleteU.jsx
+++ src/pages/DeleteU.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import { Component } from "react";
 
 class DeleteU extends Component {
-  
- 
+  state = {  } 
+  componentDelete(id) {
+    fetch('https://jsonplaceholder.typicode.com/comments/' + id, { method: 'DELETE' })
+    .then(() => this.setState({ }))
+    .then(response => console.log("‘Success", id, response));
+  }
  
   render() { 
     return (
-      <button className="btn btn1 btn-primary">Delete</button>
+      <button className="btn btn1 btn-primary" onClick={() => this.componentDelete(this.props.id)}>Delete</button>
     );
   }
 }
